daea40a899c18036db7c66314ea72789ca0e60e0
mikolajchm <chmielewski.5454@gmail.com>
2024-06-01 15:29:49 +0200

    Task 21.3

--- client/app.js
+++ client/app.js
@@ -5,33 +5,41 @@ const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+const socket = io();
+
+socket.on('message', ({ author, content }) => addMessage(author, content))
+socket.on("newUser", (userName) => {
+    addMessage("Chat Bot", userName + " has joined the conversation", true);
+});
+socket.on("userLeft", (userName) => {
+    addMessage("Chat Bot", userName + " has left the conversation... :(", true);
+});
 
 loginForm.addEventListener('submit', function(event) {
     event.preventDefault(); 
     login();
 });
 
-function login () {
-    const userNameValue = userNameInput.value.trim();
-    
-    if (!userNameValue) {
-      alert('Please enter your name.');
-      return; 
-    }
-    userName = userNameValue;
 
-    loginForm.classList.remove('show');
-  
-    messagesSection.classList.add('show');
+function login() {
+    const userNameValue = userNameInput.value.trim();
+
+    if (userNameValue === '') {
+        alert('User name cannot be empty!'); 
+    } else {
+        userName = userNameValue; 
+        socket.emit('join', userName);
+        loginForm.classList.remove('show'); 
+        messagesSection.classList.add('show');
+    }
 }
 
 addMessageForm.addEventListener('submit', function(event){
     event.preventDefault();
     sendMessage();
-
 });
 
-function sendMessage() {
+function sendMessage () {
     const messageContent = messageContentInput.value.trim();
 
     if(!messageContent) {
@@ -39,15 +47,17 @@ function sendMessage() {
         return;
     }
     addMessage(userName, messageContent);
-
+    socket.emit('message', { author: userName, content: messageContent })
     messageContentInput.value = '';
 }
 
-function addMessage(author, content) {
+
+function addMessage (author, content) {
     const message = document.createElement('li');
     message.classList.add('message');
     message.classList.add('message--received');
     if(author === userName) message.classList.add('message--self');
+    if(author === 'Chat Bot') message.classList.add('message--bot');
     message.innerHTML = `
       <h3 class="message__author">${userName === author ? 'You' : author }</h3>
       <div class="message__content">
@@ -55,4 +65,4 @@ function addMessage(author, content) {
       </div>
     `;
     messagesList.appendChild(message);
-  }
+}
--- server.js
+++ server.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const socket = require('socket.io');
 
+const server = app.listen(8000, () => {
+    console.log('Server is running on Port:', 8000)
+});
+
+const io = socket(server);
 const messages = [];
+let users = [];
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+    });
+    socket.on('disconnect', () => {
+        console.log('Oh, socket ' + socket.id + ' has left');
+        users = users.filter(user => user.id !== socket.id);
+        console.log('Updated users list:', users);
+        socket.broadcast.emit('userLeft', socket.userName);
+    });
+    socket.on('join', (login) => {
+        console.log('User joined:', login);
+        users.push({ name: login, id: socket.id });
+        console.log('Updated users list:', users);
+        socket.broadcast.emit('newUser', login);
+        socket.userName = login;
+    });
+    console.log('I\'ve added a listener on message event \n');
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.json());
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/client', 'index.html'));
-  });
-
-
-app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
 });
+
